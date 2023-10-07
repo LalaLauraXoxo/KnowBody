2c0ad9733883ef3487df78cc5878f53113f8c7f8
Espin29 <espinosaemmanuel068@gmail.com>
2023-10-07 23:38:36 +0100

    This is my first commit that isn't suicide.

---
+++ ASI.Basecode.WebApp/Controllers/EmmanuelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class EmmanuelController : Controller
+    {
+        public IActionResult Emmanuel()
+        {
+            return View();
+        }
+    }
+}
