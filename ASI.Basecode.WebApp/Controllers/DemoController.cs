8d128b19583f10657f96e5c35b1b38875416d5e9
ClarisseYvonne <clarisse.yvonne818@gmail.com>
2023-10-08 06:55:27 +0800

    jacalan commit

---
+++ ASI.Basecode.WebApp/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Jacalan()
+        {
+            return View();
+        }
+    }
+}
