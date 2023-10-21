1f092dae3b976d0d2e6d6b845489c8896691539f
CCSLAB526U27\admin27 <xymer.i.serna@gmail.com>
2023-10-22 00:18:46 +0100

    forgotpass

--- ASI.Basecode.WebApp/Controllers/ForgotPasswordController.cs
+++ ASI.Basecode.WebApp/Controllers/ForgotPasswordController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
     public class ForgotPasswordController : Controller
     {
-        public IActionResult ForgotPassword()
+        [AllowAnonymous]
+        public IActionResult ForgotPasswordView()
         {
             return View();
         }
